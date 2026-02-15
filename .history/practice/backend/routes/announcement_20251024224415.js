da94bf18d114462e07191612aafe235ffc267a81
KaguyaKawaii <stephenpatingomadero@gmail.com>
2026-02-15 23:34:38 +0800

    Update

--- .history/practice/backend/routes/announcement_20251024224415.js
+++ .history/practice/backend/routes/announcement_20251024224415.js
@@ -1,16 +1,15 @@
-// routes/announcementRoutes.js
-import express from 'express';
+const express = require('express');
 const router = express.Router();
-import * as announcementController from '../controllers/announcementController.js';
+const announcementController = require('../controllers/announcementController');
 
 // Announcement routes
 router.post('/', announcementController.createAnnouncement);
 router.get('/', announcementController.getAnnouncements);
 router.get('/active', announcementController.getActiveAnnouncements);
-router.get('/management', announcementController.getAllAnnouncementsForManagement);
+router.get('/management', announcementController.getAllAnnouncementsForManagement); // NEW ROUTE
 router.get('/:id', announcementController.getAnnouncementById);
 router.put('/:id', announcementController.updateAnnouncement);
 router.delete('/:id', announcementController.deleteAnnouncement);
 router.post('/:id/dismiss', announcementController.dismissAnnouncement);
 
-export default router;
+module.exports = router;
