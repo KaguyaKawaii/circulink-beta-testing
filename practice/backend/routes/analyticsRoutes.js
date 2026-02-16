70b9e442cb787cfc0e9450b6c274b15336520f4c
YELANIE <stephenpatingomadero@gmail.com>
2026-02-16 12:51:47 +0800

    Update analyticsRoutes.js

--- practice/backend/routes/analyticsRoutes.js
+++ practice/backend/routes/analyticsRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const analyticsController = require("../controllers/analyticsController");
-const { protect, admin } = require("../middleware/authMiddleware");
 
 // All analytics routes should be protected and admin-only
 router.use(protect, admin);
