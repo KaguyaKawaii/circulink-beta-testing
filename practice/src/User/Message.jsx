1c403d1fdb43c1fe1548513f784029c569e2267d
YELANIE <stephenpatingomadero@gmail.com>
2026-03-07 21:36:07 +0800

    Update Message.jsx

--- practice/src/User/Message.jsx
+++ practice/src/User/Message.jsx
@@ -554,7 +554,7 @@ function Message({ user, setView, currentView }) {
         <aside 
           ref={sidebarRef}
           className={`
-            fixed md:static top-0 left-0 w-72 h-full bg-white border-r border-gray-200 z-50
+            fixed md:static top-0 left-0 w-72 h-full bg-white border-r border-gray-200 z-0
             transition-transform duration-300 ease-in-out
             ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full md:translate-x-0'}
           `}
