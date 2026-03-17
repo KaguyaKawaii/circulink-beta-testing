776e66991c3d0657f7583ad1fae3fd13561d544a
YELANIE <stephenpatingomadero@gmail.com>
2026-03-17 19:00:52 +0800

    Update AdminNews.jsx

--- practice/src/Admin/AdminNews.jsx
+++ practice/src/Admin/AdminNews.jsx
@@ -981,7 +981,7 @@ function AdminNews({ setView, admin, onLogout }) {
                 <p className="text-gray-500 font-bold">Loading News...</p>
               </div>
             ) : paginatedNews.length === 0 ? (
-              <div className="text-center p-8 border border-dashed border-gray-300 rounded-lg">
+              <div className="text-center flex justify-center items-center p-8 border border-dashed border-gray-300 rounded-lg">
                 <svg className="mx-auto h-12 w-12 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                 </svg>
